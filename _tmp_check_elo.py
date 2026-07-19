faf59b3e06dee53c6ca89bbbf7ab06d870a6ef0c
davidpiontransactions-eng <david@piontransactions-eng.local>
2026-07-19 04:43:07 +0200

    chore: remove temp debug file

--- _tmp_check_elo.py
+++
@@ -1,9 +0,0 @@
-import json
-d=json.load(open("/tmp/tn2.json"))
-ms=d.get("matches",[])
-for m in ms[:5]:
-  a,b=m["playerA"],m["playerB"]
-  s=m["stats"]
-  print(f"{a['name']:25s} elo={a['elo']}  surfElo={a.get('surfaceElo','-')}  prob={m['probA']}%  eloGap={s['eloGap']}")
-  print(f"{b['name']:25s} elo={b['elo']}  surfElo={b.get('surfaceElo','-')}  prob={m['probB']}%")
-  print()
